0b875643613befa4a21a681dd90deb9d885ecf34
wasiher <watashiwaher@gmail.com>
2017-12-15 21:04:30 +0300

    Update cmdFAQ.py

--- commands/cmdFAQ.py
+++ commands/cmdFAQ.py
@@ -1,5 +1,6 @@
 def get_faq(bot, update, args):
 	import yaml
+	args = [re.sub(r'[^a-zA-Z0-9_\-$]', r'', arg)[:100] for arg in args]
 	key_max = 0
 	faq_file = 'config/faq.yml'
 	score_dict = {'':''}
