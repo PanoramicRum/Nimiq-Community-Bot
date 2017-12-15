e5e9651b5a62c1a4466d840bfe406c673406a8c3
wasiher <watashiwaher@gmail.com>
2017-12-15 21:02:32 +0300

    Update cmdNET.py

--- commands/cmdNET.py
+++ commands/cmdNET.py
@@ -2,6 +2,7 @@ def get_net(bot, update, args):
 	import json
 	import requests
 	import yaml
+	args = [re.sub(r'[^a-zA-Z0-9_\-$]', r'', arg)[:100] for arg in args]
 	dontknow = "Sorry. I don't know that information." 
 	ethdiv = 1000000000000000000
 	answer = dontknow
